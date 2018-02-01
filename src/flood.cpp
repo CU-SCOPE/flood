2955df86b43778d82dbb1af77cc9ba8c6130daf1
zschira <zasc3143@colorado.edu>
2018-02-01 14:37:07 -0700

    Added comments to main driver

--- src/flood.cpp
+++ src/flood.cpp
@@ -18,60 +18,83 @@ void FLOOD::run() {
 	unsigned int i, j, k, l;
 	quat current, temp;
 	quat rotx, roty, rotz;
-	// 45 degree rotation about each axis
+	// Define quaternion for 45 degree rotation about each axis
 	rotx.w = 0.924; roty.w = 0.924; rotz.w = 0.924;
 	rotx.x = 0.383; roty.x = 0.0;   rotz.x = 0.0;
 	rotx.y = 0.0;   roty.y = 0.383; rotz.y = 0.0;
 	rotx.z = 0.0;   roty.z = 0.0;   rotz.z = 0.383;
+	// Initialize timer
 	clock_t start, end, looking, found;
 	float error;
 	double time = 0, acq_time;
+	// Read all trajectories being tested
 	std::ifstream file(FRAME_DIRECTORIES);
     std::string dir, pos, rot; 
     while (std::getline(file, dir)) {
 #if TO_FILE
+    	// Output results
     	pos = dir + "position_act.txt";
     	rot = dir + "rotation_act.txt";
     	FILE *fpos = std::fopen(pos.c_str(), "w");
     	FILE *frot = std::fopen(rot.c_str(), "w");
 #endif
+    	// Initialize Pose to no rotation
     	q.w = 1; q.x = 0; q.y = 0; q.z = 0;
     	current = q;
     	printf("%s\n", dir.c_str());
+    	// Find the initial position
     	getPosition(dir);
+    	// Analyze trajectory
 		for(i=1; i<=NUM_FILES; i++) {
-			getFrame(i, dir);
+			// If the pose is known
 			if(!finding) {
+				// Run ICP
 				start = clock();
+				// Get current frame
+				getFrame(i, dir);
 				error = icp(scan, root, T, numPts, MAX_ITERATIONS_KNOWN);
 				end = clock();
 				time += (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
 			}
+			// If pose is unknown
 			else {
+				// Get current frame
+				getFrame(i, dir);
 				looking = clock();
+				// Remember the initial state of the LiDAR scan
 				point4D initState[numPts];
 				memcpy(initState, scan, numPts*sizeof(point4D));
 				initializePose(current, translation);
+				// First test
 				error = icp(initState, root, T, numPts, MAX_ITERATIONS_FIND);
-				for(j=0; j<7; j++) {
-					for(k=0; k<7; k++) {
-						for(l=0; l<7; l++) {
-							memcpy(initState, scan, numPts*sizeof(point4D));
-							temp = multQuat(rotz, current);
-							current = temp;
-							initializePose(current, translation);
-							error = icp(initState, root, T, numPts, MAX_ITERATIONS_FIND);
-							if(error < THRESH) {
-								k = 7; j = 7;
-								break;
+				//Rotate about each access until a solution converges
+				if(error < THRESH) {
+					for(j=0; j<7; j++) {
+						for(k=0; k<7; k++) {
+							for(l=0; l<7; l++) {
+								memcpy(initState, scan, numPts*sizeof(point4D));
+								// rotate about z
+								temp = multQuat(rotz, current);
+								current = temp;
+								initializePose(current, translation);
+								// Perform icp
+								error = icp(initState, root, T, numPts, MAX_ITERATIONS_FIND);
+								// Check for convergance
+								if(error < THRESH) {
+									k = 7; j = 7;
+									break;
+								}
 							}
+							// Rotate about y
+							temp = multQuat(roty, current);
+							current = temp;
 						}
-						temp = multQuat(roty, current);
+						// Rotate about x
+						temp = multQuat(rotx, current);
 						current = temp;
 					}
-					temp = multQuat(rotx, current);
-					current = temp;
 				}
+				// If it didn't converge send error
 				if(error > THRESH) {
 					printf("DID NOT CONVERGE!!!\n");
 					return;
@@ -80,6 +103,7 @@ void FLOOD::run() {
 				acq_time = (double) (found-looking) / CLOCKS_PER_SEC;
 				finding = false;
 			}
+			// Print results
 #if TO_FILE
 			printf("%f\n", error);
 			printTrans(T, fpos, frot);
