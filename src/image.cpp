c9a8b703d69cea0bf668a605255a27da1a096e27
zschira <zasc3143@colorado.edu>
2018-04-04 22:08:29 -0600

    only remove points outside of x (depth range) to avoid problems centering

--- src/image.cpp
+++ src/image.cpp
@@ -339,10 +339,10 @@ o3d3xx::ImageBuffer::Organize()
               pt.point[3] = 1.0f;
               if(pt.point[0] > this->position[0] + this->dims[0] || pt.point[0] < this->position[0] - this->dims[0])
                 continue;
-              if(pt.point[1] > this->position[1] + this->dims[1] || pt.point[1] < this->position[1] - this->dims[1])
+              /*if(pt.point[1] > this->position[1] + this->dims[1] || pt.point[1] < this->position[1] - this->dims[1])
                 continue;
               if(pt.point[2] > this->position[2] + this->dims[2] || pt.point[2] < this->position[2] - this->dims[2])
-                continue;
+                continue;*/
               fprintf(f, "%d  %d  %d\n", x_, y_, z_);
               
             }
